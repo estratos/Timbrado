badb435f4cae6f47d3e5aeec3674f76d23a88a9e
Estratos Consulting <julio@estratos.net>
2022-11-15 16:52:40 -0600

    Create FacturaYaSettings.php
    Create new settings

---
+++ src/Providers/FacturaYa/FacturaYaSettings.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Timbrado\Providers\FacturaYa;
+
+use Webmozart\Assert\Assert;
+
+class FacturaYaSettings
+{
+    /** @var string */
+    private $username;
+
+    /** @var string */
+    private $password;
+
+    public function __construct(string $username, string $password)
+    {
+        Assert::notEmpty($username, 'FacturaYa require a non-empty username');
+        Assert::notEmpty($password, 'FacturaYa require a non-empty password');
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function username(): string
+    {
+        return $this->username;
+    }
+
+    public function password(): string
+    {
+        return $this->password;
+    }
+}
